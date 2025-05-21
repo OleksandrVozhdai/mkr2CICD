79fd94055c11c1e8aa76699bb4f4de1b20554d01
Vozhdai Oleksandr <oleksander.vozhdai@gmail.com>
2025-05-21 17:16:25 +0300

    yml created + added reciepe on site

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from recipe.views import home
+from recipe.views import home, main, category_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', main, name='main'),
+    path('category/<int:category_id>/', category_detail, name='category_detail'),
 ]
